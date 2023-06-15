b4b44af4db80b79f16214f2709d0993c23705668
habeetech <oluhabnoni@gmail.com>
2023-06-15 12:03:35 -0700

    printf corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
+/**
+ * main - prints "comments for the
+ * printf task"
+ * Return: 0 set return
+ */
+
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
