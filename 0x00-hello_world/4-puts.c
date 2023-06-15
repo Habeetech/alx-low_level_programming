486e619a14900af084cb1fcddcc8b844c33ab260
habeetech <oluhabnoni@gmail.com>
2023-06-15 10:47:29 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
-	char puzzle []="Programming is like building a multilingual puzzle\n";
-	puts (puzzle);
+	puts ("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
