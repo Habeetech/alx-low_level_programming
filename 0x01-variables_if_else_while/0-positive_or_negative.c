d84b8d59de6df91e532027c1efa755f297fab37a
habeetech <oluhabnoni@gmail.com>
2023-06-16 09:08:18 -0700

    neg and pos

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,9 @@
 
 /* betty style doc for function main goes there */
 /**
- * main - main block
- * description: get a random number and print number
- * return 0
+ * main - "main block
+ * description: get a random number and print number"
+ * Return: 0 "always return zero"
  */
 
 
@@ -20,7 +20,7 @@ int main(void)
 if (n > 0)
 	printf("%i is positive\n", n);
 else if (n < 0)
-	printf ("%i is negative\n", n);
+	printf("%i is negative\n", n);
 else
 	printf("%i is zero\n", n);
 	return (0);
