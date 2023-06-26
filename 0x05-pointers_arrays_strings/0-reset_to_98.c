f626670215af489f667a5e5231907eba69f1834e
habeetech <oluhabnoni@gmail.com>
2023-06-26 04:26:18 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameters and updates the value its points to
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n);
+{
+	int var = 98;
+	int *ip;
+
+	_putchar('%x');
+	return (0);
+}
