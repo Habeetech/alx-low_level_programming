65037edc762c7ddc53e6fcdb4955391da9826567
habeetech <oluhabnoni@gmail.com>
2023-06-26 06:25:09 -0700

    reset upt betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98: takes a pointer to an int as parameters
+ *reset_to_98 - takes a pointer to an int as parameters
  *and updates the value its points
+ *@n:parameter to be updated
  */
 
 void reset_to_98(int *n)
