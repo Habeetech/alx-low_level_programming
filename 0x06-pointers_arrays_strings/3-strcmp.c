34bec5031082d987afd8cd83ac86f8fc6995b824
habeetech <oluhabnoni@gmail.com>
2023-07-01 00:18:42 -0700

    to7

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,30 +1,22 @@
 #include "main.h"
-
 /**
- * _strcmp - compares two string
- * @s1: first string
- * @s2: second string
- * Return: an int <, =, > if s1 is <, =, or > s2, resp.
+ * _strcmp - compare string value
+ * @s1: input value
+ * @s2: input value
+ * Return: s1[i] - s2[i]
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
+	int i;
 
-	if (*s1 < *s2)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		return (-15);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	else if (*s1 > *s2)
-	{
-		return (15);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,25 +1,19 @@
 #include "main.h"
-
 /**
- * reverse_array - reverse the content of an array of int
- * @a: pointer to the array
- * @n: number of elements of the array
- * Return: None
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ * Return: void
  */
-
 void reverse_array(int *a, int n)
 {
-	int start = 0;
-	int end = n - 1;
+	int i;
+	int t;
 
-	while (start < end)
+	for (i = 0; i < n--; i++)
 	{
-		int temp = a[start];
-
-		a[start] = a[end];
-		a[end] = temp;
-		start++;
-		end--;
-
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
 	}
 }
