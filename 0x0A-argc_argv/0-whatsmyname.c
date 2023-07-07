330273cc487fccc334acdc6c814c08f99b73ad98
habeetech <oluhabnoni@gmail.com>
2023-07-07 01:56:14 -0700

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_name - prints its own name
+ *@argc: argument count
+ *@argv: argument variable
+ *Return: Always 0
+ */
+
+int print_name(int argc, char* argv[])
+{
+	int i, j;
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			_putchar(argv[i][j]);
+		}
+		_putchar('\n');
+	}
+	return 0;
+}
