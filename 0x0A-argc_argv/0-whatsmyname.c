6f87a9588ba5acbb0114794b1310dc4af7cb8275
habeetech <oluhabnoni@gmail.com>
2023-07-07 05:12:26 -0700

    re myname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,15 @@
 #include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - output code
+ * @c: character to print
+ * Return: Success
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  *print_name - prints its own name
@@ -19,5 +30,17 @@ int print_name(int argc, char *argv[])
 		}
 		_putchar('\n');
 	}
+	return (0);
+}
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument variable
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	print_name(argc, argv);
 	return (0);
 }
