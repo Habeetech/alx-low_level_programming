4266f7b641eff9c30713e28873b6afc04d239bf6
habeetech <oluhabnoni@gmail.com>
2023-07-17 05:00:15 -0700

    dog upt

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - a structure to describe dogs
+ * @name: name of the dog
+ * @age: the dog's age
+ * @owner: the dog's owner
+ * Return: None
+ */
+
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
+
+#endif
