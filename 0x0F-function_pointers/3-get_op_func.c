ff32e09f0ba0ff2a689cd6934338b3efbf559ac6
habeetech <oluhabnoni@gmail.com>
2023-07-19 13:50:38 -0700

    calc upt

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
+#include "3-calc.h"
 #include <stdlib.h>
 #include <string.h>
-#include "3-calc.h"
 /**
- * get_op_func - select function to perform based on the operator passed
- * @s: operator passed as argument to the program
- * Return: pointer to the function corresponding to the operator passed
- * if 's' does not match any of the operators, return NULL
+ * get_op_func - ...
+ * @s: ...
+ * Return: ...
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -19,13 +17,12 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 	int i = 0;
-	op_t op = ops[i++];
 
-	while (op.op != NULL)
+	while (i < 5)
 	{
-		if (strcmp(s, op.op) == 0)
-			return (op.f);
-		op = ops[i++];
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
 	}
-	return (NULL);
+	return (0);
 }
