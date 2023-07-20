213a9a844e4d5328ff5e199a875d67eb156e4aea
habeetech <oluhabnoni@gmail.com>
2023-07-20 10:12:53 -0700

    print all upt

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,48 +1,49 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
 /**
- * print_all - prints anything followed by a new line
- * @format: a list of type of args
- * Return: None
+ * print_all - prints all of the agruments when specified
+ * @format: specifies the necessary operations
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i = 0;
+	int i;
+	int flag;
 	char *str;
-	int num;
-	char character;
-	float fnum;
-	
-	va_start(args, format);
-	while (format && format[i])
-	{
-		if (format[i] == 'c')
-				{
-					character = va_arg(args, int);
-					printf("%c", character);
-				}
-		else if (format[i] == 'i')
-		{
-			num = va_arg(args, int);
-			printf("%d", num);
-		}
-		else if (format[i] == 'f')
-		{
-			fnum = (float)va_arg(args, double);
-			printf("%f", fnum);
-		}
-		else if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (str == NULL) printf("(nil)");
-			else printf("%s", str);
-		}
-		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')) printf(", ");
+	va_list a_list;
 
+	va_start(a_list, format);
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
+				break;
+			case 's':
+				str = va_arg(a_list, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
+			default:
+				flag = 1;
+				break;
+		}
+		if (format[i + 1] != '\0' && flag == 0)
+			printf(", ");
 		i++;
 	}
-	va_end(args);
 	printf("\n");
+	va_end(a_list);
 }
