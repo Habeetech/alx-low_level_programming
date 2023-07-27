29be2c281e04ba35660a0c616bbb8611028d4220
habeetech <oluhabnoni@gmail.com>
2023-07-27 11:11:33 -0700

    first print

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * first_print - prints before the main function
+ * Return: None
+ */
+void first_print()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
