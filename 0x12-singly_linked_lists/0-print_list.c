7e570ddaf71635612da4288cccdf9142fe2fa4a6
habeetech <oluhabnoni@gmail.com>
2023-07-27 05:38:14 -0700

    print list upt

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,11 +11,6 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-	{
-		printf("[0] (nil)\n");
-		return (0);
-	}
 	while (h != NULL)
 	{
 		printf("[%d] %s\n", h->len, h->str != NULL ? h->str : "(nil)");
