1e38a92147714a5c989c74336975316c2c4fef95
habeetech <oluhabnoni@gmail.com>
2023-07-27 11:15:54 -0700

    betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  * first_print - prints before the main function
  * Return: None
  */
-void first_print()
+void first_print(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 	first_print();
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
 }
