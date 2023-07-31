c0f51231a8f36030352dbefb5555f1c72c44dfa8
habeetech <oluhabnoni@gmail.com>
2023-07-31 05:18:37 -0700

    sum of list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_listint - returns sum of listint_t list
+ * @head: pointer to the first element
+ * Return: 0 if list is empty
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return sum;
+}
